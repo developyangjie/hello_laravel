1d716fce91168448e112207ed8291bda455902b0
yangjie <875241784@qq.com>
2017-11-28 06:27:08 +0000

    3

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.default")
+@section("title","help")
+
+@section("content")
+<h1>帮忙噻</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.default")
+@section("title","首页")
+
+@section("content")
+    <h1>welcome</h1>
+@stop
